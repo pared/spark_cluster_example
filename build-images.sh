f7bfc4fb39dea34cfbf2fb66f2dcbc97b05e8c55
pared <pawelredzynski@gmail.com>
2019-05-14 23:03:55 -0700

    cleanup

--- build-images.sh
+++ build-images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build . -t spark-base:2.3.1 -f base_D
-docker build . -t spark-master:2.3.1 -f master_D
-docker build . -t spark-worker:2.3.1 -f worker_D
-docker build . -t spark-submit:2.3.1 -f submit_D
+docker build base -t spark-base:2.3.1
+docker build master -t spark-master:2.3.1 
+docker build worker -t spark-worker:2.3.1 
+docker build submit -t spark-submit:2.3.1
